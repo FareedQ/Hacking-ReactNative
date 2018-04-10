3c233ca6fb7b4dc40c646daab3353c944e2acda7
Fareed's Personal Account <Personal@Fareeds-MacBook-Pro.local>
2018-04-10 11:59:35 -0400

    learn the basics of styles

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ export default class App extends React.Component {
         <Greeting name='Rexxar' />
         <Greeting name='Jaina' />
         <Greeting name='Valeera' />
+        <Blink text='I love to blink' />
       </View>
     );
   }
@@ -28,6 +29,27 @@ class Greeting extends Component {
   }
 }
 
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
+    return (
+      <Text style={styles.bigblue}>{display}</Text>
+    );
+  }
+}
+
 AppRegistry.registerComponent('AwesomeProject', () => App);
 
 const styles = StyleSheet.create({
@@ -36,5 +58,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
   },
 });
