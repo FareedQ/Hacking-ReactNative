52847a0cc9110513d2fc2b55c4b9f843615991e9
Fareed's Personal Account <Personal@Fareeds-MacBook-Pro.local>
2018-04-10 11:48:44 -0400

    added images based on tutorial

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
       <View style={styles.container}>
+        <Image source={pic} style={{width: 193, height: 110}}/>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
@@ -12,6 +16,8 @@ export default class App extends React.Component {
     );
   }
 }
+
+AppRegistry.registerComponent('AwesomeProject', () => App);
 
 const styles = StyleSheet.create({
   container: {
