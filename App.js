6b18266f584c539ef5dc7c397fc14e3be4258015
Fareed's Personal Account <quraishifareed@gmail.com>
2018-04-13 11:34:24 -0400

    refactored: removed excessive styles form the App.js file

--- App.js
+++ App.js
@@ -23,74 +23,7 @@ export default class NavController extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  contentView: {
-   flex: 1,
-   paddingTop:60,
-  },
   container: {
    flex: 1,
   },
-  myButton: {
-  	padding: 20,
-  	backgroundColor: '#b2f333',
-  	borderWidth: 2,
-  	borderColor: '#cccccc',
-  },
-  buttonContainer: {
-    margin: 20
-  },
-  alternativeLayoutButtonContainer: {
-    margin: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  bigblue: {
-    color: 'skyblue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-  button: {
-    marginBottom: 30,
-    width: 260,
-    alignItems: 'center',
-    backgroundColor: '#2196F3'
-  },
-  buttonText: {
-    padding: 20,
-    color: 'white'
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
-  },
-  loginScreenButton:{
-    marginRight:40,
-    marginLeft:40,
-    marginTop:10,
-    paddingTop:10,
-    paddingBottom:10,
-    backgroundColor:'#1E6738',
-    borderRadius:10,
-    borderWidth: 1,
-    borderColor: '#fff',
-  },
-  loginText:{
-      color:'#fff',
-      textAlign:'center',
-      paddingLeft : 10,
-      paddingRight : 10
-  }
 });
--- ContentView.js
+++ ContentView.js
@@ -7,33 +7,99 @@ import {
   TextInput,
   View,
   Button,
+  FlatList, 
+  TouchableHighlight, 
 } from 'react-native';
 import hello from './hello';
 
 export default class ContentView extends Component<{}> {
-	constructor(props) {
-  	  super(props);
-  	  this.state = {};
-	}
 
-	_onSearchPressed = () => {
+	_navigateToNextPage = () => {
 	    this.props.navigator.push({title: 'Results', component: hello, passProps: {}});
+	};
+	
+	_renderItem = ({item, index}) => (
+		<ListItem item={item} index={index} onPressItem={this._navigateToNextPage} />
+	);
+  
+	_onPressItem = (index) => {
+		console.log("Pressed row: "+index);
 	};
 
   render() {
     return (
       <View style={styles.container}>
-      	<Text>Hey</Text>
-        <Button onPress={this._onSearchPressed} color='#48BBEC' title='Go' />
+        <View style={styles.rowContainer}>
+  		 <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={this._renderItem}
+        />
+        </View>
       </View>
+    );
+  }
+}
+
+class ListItem extends React.PureComponent {
+  _onPress = () => {
+    this.props.onPressItem(this.props.index);
+  }
+
+  render() {
+    const item = this.props.item;
+    return (
+        <View>
+          <View style={styles.rowContainer}>
+            <TouchableHighlight onPress={this._onPress} underlayColor='#dddddd' style={styles.buttonContainer}>
+              <Text style={styles.title}
+                numberOfLines={1}>{item.key}</Text>
+            </TouchableHighlight>
+          </View>
+          <View style={styles.separator}/>
+        </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 30,
-    marginTop: 65,
-    alignItems: 'center'
+    alignItems: 'center',
+    flex:1,
   },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  title: {
+    fontSize: 20,
+    color: '#fff'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+  buttonContainer: {
+  	backgroundColor: 'green',
+  	flex: 1,
+  	margin: 10,
+  	padding: 20,
+  	alignItems: 'center',
+  	borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#656565',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+  },
+  
 });
